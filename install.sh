6dc0c3cd2d201ebd3fa1de808cd01784c752eeb6
Christophe Servan <christophe.servan@gmail.com>
2019-06-04 19:33:13 +0200

    Finalisation of automatic installation with the install script

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 export PREFIX=/usr/local/
+
 if [ -n "$1" ]
 then
     export PREFIX=$1
+fi 
 
 echo "Prefix set to $PREFIX"
-
 export CMAKE_PREFIX_PATH=$PREFIX
+
+export CXXFLAGS="-I$PREFIX/include -L$PREFIX/lib"
 
 git submodule update --init --recursive
 
@@ -27,6 +30,6 @@ mkdir -p $PREFIX
 rm -rf build
 mkdir -p build
 pushd build
-	cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_BUILD_TYPE=Release 
+	cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_PREFIX_PATH="${PREFIX}" -DCMAKE_BUILD_TYPE=Release 
 	make -j 4 && make install
 popd

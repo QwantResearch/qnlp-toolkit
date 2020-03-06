fbf6749795cdea1422a898fc4c6c4d634bbaaa7b
Christophe Servan <christophe.servan@gmail.com>
2020-03-06 11:43:38 +0100

    Added english processing

--- include/en_tokenizer.h
+++ include/en_tokenizer.h
@@ -12,6 +12,10 @@ namespace qnlp {
                 Tokenizer (syntax, lowercased, underscore, dash, aggressive, nopunct){lang="en";};
         protected:
             bool process_lang(vector<std::__cxx11::wstring> & vecwtoken) override;
+            const vector<wstring> wlang_process = {L"'s", L"'t", L"'ll",
+            };     
+            const vector<wstring> wlang_process_not = {L"don't", L"isn't", L"didn't",
+            };     
     };
 }
 
--- src/en_tokenizer.cpp
+++ src/en_tokenizer.cpp
@@ -4,8 +4,29 @@ using namespace qnlp;
 
 bool Tokenizer_en::process_lang (vector<wstring>& vecwtoken)
 {
-    return true;
+    auto vecwtoken_it=vecwtoken.begin();
+    auto vecwtoken_it_prev=vecwtoken.begin();
+    if (vecwtoken_it != vecwtoken.end()) vecwtoken_it++; else return false;
+    wstring toTest;
+    while (vecwtoken_it != vecwtoken.end() )
+    {
+        toTest=(*vecwtoken_it_prev)+(*vecwtoken_it);
+        if (std::find(wlang_process.begin(), wlang_process.end(), toTest) != wlang_process.end()) // && (*vecwtoken_it) == L"."
+        {
+            (*vecwtoken_it_prev)=toTest;
+            (*vecwtoken_it)=L"";
+            return true;
+        }
+        if (std::find(wlang_process_not.begin(), wlang_process_not.end(), toTest) != wlang_process_not.end() && ((int)(*vecwtoken_it).size()>0 && (*vecwtoken_it)[0]==u'\'')) // && (*vecwtoken_it) == L"."
+        {
+            wcerr << (*vecwtoken_it_prev) << "|" << (*vecwtoken_it) << endl;
+            (*vecwtoken_it_prev)=toTest.substr(0,toTest.find(u'n'));
+            (*vecwtoken_it)=L"n't";
+            return true;
+        }
+        vecwtoken_it++;
+        vecwtoken_it_prev++;
+    }
+    return false;
 }
-
-
 
